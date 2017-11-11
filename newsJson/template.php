a85d809e4ded1cfefc981daf5870098347ef8b43
ptflp <filippov.pa@s-vfu.ru>
2017-11-12 00:00:21 +0900

    json only

--- newsJson/template.php
+++ newsJson/template.php
@@ -1,6 +1,4 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-<pre>
-<?php 
-echo json_encode($arResult["ITEMS"],true);
+<?php
+echo json_encode($arResult,true);
 ?>
-</pre>
